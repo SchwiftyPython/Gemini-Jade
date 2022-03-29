5a9dee5a97979c901ee8beec3d33668bb41f08af
brian sauro <banana@bananahammergames.com>
2022-03-29 14:43:12 -0400

    Added additional fields to BodyTemplate. Added Neck and all child parts to Human Body Template. Added types for part Height and Depth.

--- Assets/Scripts/World/Pawns/BodyTemplates/BodyTemplate.cs
+++ Assets/Scripts/World/Pawns/BodyTemplates/BodyTemplate.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Assets.Scripts.World.Pawns.BodyPartGroupTemplates;
 using Assets.Scripts.World.Pawns.BodyPartTemplates;
 using Sirenix.OdinInspector;
 using Sirenix.Serialization;
@@ -10,17 +11,29 @@ namespace Assets.Scripts.World.Pawns.BodyTemplates
     [CreateAssetMenu(menuName = "Templates/BodyTemplate")]
     public class BodyTemplate : Template
     {
-        private string _selfTabLabel;
-
         public struct Part
         {
             [TabGroup("Tabs", "$selfTabLabel")]
             [LabelText("@self?.name")]
             public BodyPartTemplate self;
 
+            [FoldoutGroup("Tabs/$selfTabLabel/Attributes")]
             public string selfTabLabel;
 
-            //todo need scriptable objects for height and depth
+            [FoldoutGroup("Tabs/$selfTabLabel/Attributes")]
+            public string customLabel;
+
+            [FoldoutGroup("Tabs/$selfTabLabel/Attributes")]
+            public BodyPartHeight.BodyPartHeight height;
+
+            [FoldoutGroup("Tabs/$selfTabLabel/Attributes")]
+            public BodyPartDepth.BodyPartDepth depth;
+
+            [FoldoutGroup("Tabs/$selfTabLabel/Attributes")]
+            public List<BodyPartGroupTemplate> groups;
+
+            [FoldoutGroup("Tabs/$selfTabLabel/Attributes")]
+            public float coverage;
 
             [NonSerialized, OdinSerialize]
             [TabGroup("Tabs", "Children")]
