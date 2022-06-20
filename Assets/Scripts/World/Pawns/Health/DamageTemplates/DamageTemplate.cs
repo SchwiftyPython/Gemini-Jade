498c80c2a528be6842bf0efccc35c464932d4813
brian sauro <banana@bananahammergames.com>
2022-06-20 13:38:06 -0400

    Added DamageInfo.cs Added DamageResult.cs Added DamageTemplate.cs Added DamageWorker.cs

---
+++ Assets/Scripts/World/Pawns/Health/DamageTemplates/DamageTemplate.cs
@@ -0,0 +1,70 @@
+using System;
+using Assets.Scripts.World;
+using UnityEngine;
+using World.Pawns.Health.DamageWorkers;
+using World.Pawns.Health.HealthModifiers;
+
+namespace World.Pawns.Health.DamageTemplates
+{
+    public class DamageTemplate : Template
+    {
+        private DamageWorker _worker;
+        
+        public Type workerClass = typeof(DamageWorker);
+
+        public bool isAttack;
+
+        public bool knocksBack = true;
+
+        public bool damageHp = true;
+
+        public bool interrupt = true;
+
+        public bool ranged;
+
+        public float buildingDamageMod = 1f;
+
+        public float plantDamageMod = 1f;
+        
+        //todo sounds
+
+        public string deathMessage = "{0} has been killed.";
+        
+        //todo visual effects
+
+        public int baseDamage = -1;
+
+        public float baseArmorPen = -1f;
+
+        public float baseStoppingPower;
+        
+        //todo additional health mods
+
+        public HealthModTemplate healthModTemplate;
+
+        public bool explodes;
+
+        public float explosionHeatPerTile;
+        
+        //todo explosion sound
+
+        public AnimationCurve hitMultipleTargetsCurve;
+        
+        //todo maybe stun chance and duration
+
+        public DamageWorker Worker
+        {
+            get
+            {
+                if (_worker == null)
+                {
+                    _worker = (DamageWorker)Activator.CreateInstance(workerClass);
+                    
+                    //todo worker template = this
+                }
+
+                return _worker;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/World/Pawns/Health/DamageWorkers/DamageWorker.cs
@@ -0,0 +1,7 @@
+namespace World.Pawns.Health.DamageWorkers
+{
+    public class DamageWorker
+    {
+        //todo we can probably start to deviate here and come up with our own thing
+    }
+}
